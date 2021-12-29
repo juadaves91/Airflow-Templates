b1f2bc6ca26a99616af3cd01064f3a502f93912b
juadaves91 <juadaves.1991@gmail.com>
2021-12-29 13:02:23 -0500

    Adding varible_dag.py

--- variable_dag.py
+++ variable_dag.py
@@ -9,9 +9,9 @@ def display_variable():
     return my_var
 
 def display_variable_env():
-    my_var = Variable.get("my_var_enviroment")
-    print('variable' + my_var)
-    return my_var
+    my_var2 = Variable.get("my_var_enviroment")
+    print('variable' + my_var2)
+    return my_var2
 
 dag = DAG(dag_id="variable_dag", start_date=datetime(2021, 1, 1),
     schedule_interval='@daily', catchup=False)
