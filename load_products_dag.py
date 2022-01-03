74428788ff0b9015fdf881ea0a57635bc05fe1e7
juadaves91 <juadaves.1991@gmail.com>
2022-01-03 12:22:02 -0500

    change name custom operator

--- load_products_dag.py
+++ load_products_dag.py
@@ -5,12 +5,20 @@ from airflow import DAG
 
 # Operators; we need this to operate!
 from operator_s3_to_postgres import S3ToPostgresTransfer
+from airflow.operators.python_operator import PythonOperator
 
+
+def print_welcome():
+    return 'Welcome from custom operator - Airflow DAG!'
 
 dag = DAG('dag_insert_data', 
           description='Hello World DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
+
+welcome_operator = PythonOperator(task_id='hello_task', 
+                                python_callable=print_welcome, 
+                                dag=dag)
 
 process_dag = S3ToPostgresTransfer(
     task_id = 'dag_s3_to_postgres',
@@ -23,4 +31,4 @@ process_dag = S3ToPostgresTransfer(
     dag = dag
 )
 
-process_dag
+welcome_operator >> process_dag
